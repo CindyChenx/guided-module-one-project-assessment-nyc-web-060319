9087a9aaf436e4562d19f2c60f541124cb733c01
Tori Sigler <mssigler@gmail.com>
2019-06-17 17:06:36 -0400

    Changed to output hash with ISO country as key

--- holidays_countries.rb
+++ holidays_countries.rb
@@ -4,15 +4,15 @@
 	require 'pry'
 
 
-	$the_world = ["af", "al"]
-	$holidays_of_the_world = []
+	$the_world = ["af", "al", "dz", "as", "ad", "ao", "ai", "ag", "ar", "am", "aw", "au", "at", "az"]
+	$holidays_of_the_world = {}
 	$sleep_time = (rand(2000..20000))/1000
 
 	def get_holidays_for_country(list)
 		list.each do |c|
-			$holidays_of_the_world << c
+			#$holidays_of_the_world << c #Change to Hash
 			response = RestClient.get("https://calendarific.com/api/v2/holidays?&api_key=69c17a11b1490a34725e55693d121a624e7f8c02&country=#{c}&year=2019")
-			$holidays_of_the_world << JSON.parse(response)
+			$holidays_of_the_world[c.to_sym] = JSON.parse(response)
 			sleep($sleep_time)
 		end
 	end
@@ -22,6 +22,9 @@
 
 	puts "Records are: "
 	puts $holidays_of_the_world
+	puts
+	puts
+	puts
 	puts "END"
 
 
