cc1d439a33aa6346e311394f49a0aa2ec1b29437
te68 <te68@cornell.edu>
2019-06-18 16:00:46 -0400

    all the holidays are in the db now

--- bin/run.rb
+++ bin/run.rb
@@ -1,2 +1,11 @@
 require_relative '../config/environment'
-require_relative '../populate_hol_db.rb'
+require_relative '../Seeding_Stuff/populate_hol_db.rb'
+
+
+# holidayArray = getHolidays(HOLIDAYS_OF_THE_WORLD)
+# # i = 0
+# # while i < holidayArray.length
+# #   puts holidayArray[i].inspect
+# #   i += 50
+# # end
+# puts holidayArray.last.inspect
