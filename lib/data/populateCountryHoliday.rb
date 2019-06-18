ba1c2a6812bef96d0c7b776b8c0ff81e43dd7da1
Tori Sigler <mssigler@gmail.com>
2019-06-18 14:56:13 -0400

    Corrected H countries file

--- lib/data/populateCountryHoliday.rb
+++ lib/data/populateCountryHoliday.rb
@@ -1,6 +1,8 @@
 class PopulateCountryHoliday
 
-
+	#require_relative './data_structure_to_parse.rb'
+	# require_relative './example_data.rb'
+	# require_relative './populate_CountryHoliday_table_from_arrays.rb'	
 	require 'pry'
 
 	# populate join table.  Holidays <> Countries. 
@@ -37,11 +39,29 @@ class PopulateCountryHoliday
 
 
 
-		#US only data for sampling
-	#holidays_of_the_world = ["us", {meta... }]
+
+
+	seed_array = find_country_holiday_pairs(_data_)
+	puts "Seed Array is  :  #{seed_array}"
+
+	seed_array.each_with_index do |elements_index|
+		if elements_index % 2 == 1
+			country_id_for_db = Country.where(:country_code => elements_index).first.id
+		else 
+			holiday_id_for_db = Holiday.where(:name => elements_index).first.id
+			CountryHoliday.create(:country_id => country_id_for_db, :holiday_id => holiday_id_for_db )
+		end
+	end
+	
+
+	# 	# add logic to check is ARRAY ODD =/= country_id_for_db before search DB again
+	# country_id_for_db = Country.where(:ISO JAWN => ARRAY ODD).first.id
 
 
 
-	find_country_holiday_pairs(ExampleData.holidays_of_the_world)
+
+
+
+
 
 end #End class
--- lib/data/populate_CountryHoliday_table_from_arrays.rb
+++ lib/data/populate_CountryHoliday_table_from_arrays.rb
@@ -1,16 +1,31 @@
+class PopulateCountryHolidayTableFromArrays
+
 # To populate CountryHoliday Table
 # Loop through array of pairs of holiday name + country code, 
 # Look up id for each in DB, 
 # Then create new row in join table
 # !!! Need to rename this to match ActiveRecord rules. 
 
-require_relative '../config/environment'
+	#require_relative './data_structure_to_parse.rb'
+	# require_relative './example_data.rb'
+	# require_relative './populateCountryHoliday.rb'
+	#require_relative './populate_CountryHoliday_table_from_arrays.rb'	
+	#require_relative '../config/environment'
 
-			seed_array = find_country_holiday_pairs
 
-			holiday_id_for_db = Holiday.where(:name => ARRAY EVEN).first.id
 
-			# add logic to check is ARRAY ODD =/= country_id_for_db before search DB again
-			country_id_for_db = Country.where(:ISO JAWN => ARRAY ODD).first.id
+	
 
-			CountryHoliday.create(:country_id => country_id_for_db, :holiday_id => holiday_id_for_db )
+			# seed_array = find_country_holiday_pairs($holidays_of_the_world)
+
+
+			# holiday_id_for_db = Holiday.where(:name => ARRAY EVEN).first.id
+
+			# 	# add logic to check is ARRAY ODD =/= country_id_for_db before search DB again
+			# country_id_for_db = Country.where(:ISO JAWN => ARRAY ODD).first.id
+
+			# CountryHoliday.create(:country_id => country_id_for_db, :holiday_id => holiday_id_for_db )
+
+
+
+end #class
