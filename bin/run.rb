b94e7bd2f1eeb34db85a68d9ae440d25c51712d6
Tori Sigler <mssigler@gmail.com>
2019-06-19 10:42:30 -0400

    Re-Tested CountryHoliday populate code

--- bin/run.rb
+++ bin/run.rb
@@ -15,20 +15,27 @@ require_relative './interface_controller.rb'
 	# 	Code to test populate Country<>Holiday join table - Works! 
 	# def populate_db_holidays_countries(array)
 	# country_id_for_db = nil
+	# prior_holiday = 0 
 	# holiday_id_for_db = nil
 	# 	array.each_with_index do |element, elements_index|
+	# 		#puts "Running index 0..24.   Current index is:  #{elements_index}"
 	# 		if elements_index % 2 == 0
-	# 			country_id_for_db = Country.where(:country_code => "#{element}")[0].id
+	# 			country_id_for_db = Country.where(:country_code => "#{element}")[0].id 		#was just [0].id
 	# 		else 
-	# 			if element != holiday_id_for_db 
+	# 			#puts "TESTING IF STATEMENT : Element is: #{element} & prior_holiday is #{prior_holiday}"
+	# 			if element != holiday_id		# was if element != holiday_id.. 
+	# 				#puts "Element is: #{element} & prior_holiday is #{prior_holiday}"
+	# 				#puts "prior_holiday is #{prior_holiday} & holiday_id_for_db is #{holiday_id_for_db} !"
 	# 				holiday_id_for_db = Holiday.where(:name => "#{element}")[0].id
+	# 				prior_holiday = element
+	# 				#puts "holiday_id_for_db is : #{holiday_id_for_db}"
 	# 				CountryHoliday.create(country_id: country_id_for_db, holiday_id: holiday_id_for_db )
 	# 			end
 	# 		end
 	# 	end
 	# end
 	# seed_array = find_country_holiday_pairs(HOLIDAYS_OF_THE_WORLD)
-	# populate_db_holidays_countries(seed_array[0..24])
+	# populate_db_holidays_countries(seed_array)
 
 
 
