d568eaf620b041696183d7f7eae957d8a14ff42c
Tori Sigler <mssigler@gmail.com>
2019-06-19 09:21:56 -0400

    Code to populate HolidayCountry join table tested and good.

--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,6 @@
 require_relative '../config/environment'
 require_relative '../Seeding_Stuff/populate_hol_db.rb'
-
+require_relative "../lib/data/seeds_prototype_holidayscountries.rb"
 
 # holidayArray = getHolidays(HOLIDAYS_OF_THE_WORLD)
 # # i = 0
@@ -9,3 +9,27 @@ require_relative '../Seeding_Stuff/populate_hol_db.rb'
 # #   i += 50
 # # end
 # puts holidayArray.last.inspect
+
+
+
+	def populate_db_holidays_countries(array)
+	country_id_for_db = nil
+	holiday_id_for_db = nil
+		array.each_with_index do |element, elements_index|
+			if elements_index % 2 == 0
+				country_id_for_db = Country.where(:country_code => "#{element}")[0].id
+			else 
+				if element != holiday_id_for_db 
+					holiday_id_for_db = Holiday.where(:name => "#{element}")[0].id
+					CountryHoliday.create(country_id: country_id_for_db, holiday_id: holiday_id_for_db )
+				end
+			end
+		end
+	end
+
+	seed_array = find_country_holiday_pairs(HOLIDAYS_OF_THE_WORLD)
+
+	populate_db_holidays_countries(seed_array[0..24])
+
+
+	
--- lib/data/seeds_prototype_holidayscountries.rb
+++ lib/data/seeds_prototype_holidayscountries.rb
@@ -11,17 +11,15 @@
 	def find_country_holiday_pairs(data)
 		all_array = []
 
-		data.each do |country, server_replies|		#CountryHoliday.create(country_id: , holiday_id: )
-			server_replies.each do |server_reply, server_datas| #server_datas = holidays
+		data.each do |country, server_replies|							#CountryHoliday.create(country_id: , holiday_id: )
+			server_replies.each do |server_reply, server_datas| 		#server_datas = holidays
 				if server_reply == "response"
 					server_datas.each do |holiday_key, holiday_values |
 						holiday_values.each do |holiday_array_element|
-							holiday_array_element.each do |holiday_hash, holiday_datas| #was holiday_values[0]
+							holiday_array_element.each do |holiday_hash, holiday_datas| 
 								if holiday_hash == "name"
 										all_array << country
-										all_array << holiday_datas
-										#puts "Holiday_datas is : #{holiday_datas},  country is: #{country}"	
-					
+										all_array << holiday_datas					
 								end
 							end
 						end
@@ -49,15 +47,16 @@
 	holiday_id_for_db = nil
 		array.each_with_index do |element, elements_index|
 			if elements_index % 2 == 0
-				country_id_for_db = Country.where(:country_code => element).first.id
+				country_id_for_db = Country.where(:country_code => "#{element}")[0].id
 			else 
 				if element != holiday_id_for_db 
-					holiday_id_for_db = Holiday.where(:name => element).first.id
-					CountryHoliday.create(:country_id => country_id_for_db, :holiday_id => holiday_id_for_db )
+					holiday_id_for_db = Holiday.where(:name => "#{element}")[0].id
+					CountryHoliday.create(country_id: country_id_for_db, holiday_id: holiday_id_for_db )
 				end
 			end
 		end
 	end
+
 
 
 
