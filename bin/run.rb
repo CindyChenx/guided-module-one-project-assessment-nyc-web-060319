730db4f11b63beee9592825d38c58f09e780f0e1
CindyChenx <chen_cx0128@hotmail.com>
2019-06-19 09:29:19 -0400

    test some cli method

--- bin/run.rb
+++ bin/run.rb
@@ -1,2 +1,16 @@
 require_relative '../config/environment'
 require_relative '../Seeding_Stuff/populate_hol_db.rb'
+require_relative './interface_controller.rb'
+
+
+
+# jacklyn = User.all.last
+
+# puts jacklyn.inspect
+
+
+# #trip1 = Trip.create(name: "trip1", start_date: "2019-01-01", end_date: "2019-10-10", user_id: 20)
+# trip1 = jacklyn.create_trip("trip1", "2019-01-01", "2019-10-10")
+# #jacklyn.update_trip_name("trip1", "Spring Break")
+# puts trip1.inspect
+
